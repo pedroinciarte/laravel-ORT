5e94be7ff8408de57e7f6d201f73dd3eba776cca
pedroinciarte <pedro.inciarte13@gmail.com>
2025-05-28 22:49:00 -0300

    Add login method and route

--- routes/auth.php
+++ routes/auth.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
 
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
