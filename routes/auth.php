3f700131e3bce339656b7c89a32f0ab7d48ad2fe
pedroinciarte <pedro.inciarte13@gmail.com>
2025-05-28 23:06:37 -0300

    logout method

--- routes/auth.php
+++ routes/auth.php
@@ -4,4 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
 
 Route::post('/register', [AuthController::class, 'register']);
+
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
